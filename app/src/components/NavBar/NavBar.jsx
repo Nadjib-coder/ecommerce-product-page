4c9b1d7dc38ee1d0ff7c0d17fbe1dddcc5c05b74
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-06-15 21:09:30 +0100

    latest update contain: - remove all the previews code and start from scracth why?:   - becuase i faced some problem in tailwind css so i decided to work with pure css to gain more skills - create the navbar from scratch - style it - work on the hover over the links - start working on the small screen device navbar Next step: - complet navbar for small screen device styling

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -2,34 +2,41 @@ import './NavBar.css';
 
 export default function NavBar() {
   return (
-    <nav>
-      <div className="navbar">
-        <div className="left-side">
-          <img className="sneakers" alt="sneakers" src="/assets/logo.svg" />
-          <a href="#">Collections</a>
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
+    <header className="nav-container" id="home">
+      <nav className="navbar">
+        <div className="logo-menu-section">
+          <img className="menu" src="/assets/icon-menu.svg" alt="Menu" />
+          <a href="#home">
+            <img src="/assets/logo.svg" alt="Logo" />
+          </a>
         </div>
-        <div className="right-side">
-          <button className="cart">
-            <img
-              className="shopping-bag-icon"
-              alt="shopping bag"
-              src="/assets/icon-cart.svg"
-            />
-          </button>
-          <button className="profile">
-            <img
-              className="avatar"
-              alt="Avatar"
-              src="/assets/image-avatar.png"
-            />
-          </button>
+        <div className="mobile-menu-active"></div>
+        <div className="links-section">
+          <img className="close" src="/assets/icon-close.svg" alt="close" />
+          <ul className="links">
+            <li>
+              <a href="#">Collections</a>
+            </li>
+            <li>
+              <a href="#">Men</a>
+            </li>
+            <li>
+              <a href="#">Women</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
         </div>
-      </div>
+        <div className="cart-avatar-section">
+          <img className="cart" src="/assets/icon-cart.svg" alt="Cart" />
+          <img className="avatar" src="/assets/image-avatar.png" alt="Avatar" />
+        </div>
+      </nav>
       <hr />
-    </nav>
+    </header>
   );
 }
