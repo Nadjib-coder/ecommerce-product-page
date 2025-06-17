aa04d23c25c21f61c8da10d86f4d0ecf6e7abf82
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-06-17 17:50:51 +0100

    update: -start the product page section

---
+++ app/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <section>
+      <section className="product-images">
+        <img src=""></img>
+        <div className=""></div>
+      </section>
+      <section className="product-information"></section>
+    </section>
+  );
+}
