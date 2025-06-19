af59eefef7822778f995a4470efca9222b87938f
Nadjib-coder <nadjibmouhoun1@gmail.com>
2025-06-19 02:15:59 +0100

    update contain: -install tailwind to use it the product page section - create the left side image gallery section with react and style it with tailwind next step: - create the ProductDetials 'right section'

--- app/src/components/ProductPage/ImageGallery.jsx
+++ app/src/components/ProductPage/ImageGallery.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react';
+import { images, thumbnail } from './images';
+
 const ImageGallery = () => {
-  const thumbnail = [
+  const [activeImage, setActiveImage] = useState('/assets/image-product-1.jpg');
+  const [activeThumbnail, setActiveThumbnail] = useState(
     '/assets/image-product-1-thumbnail.jpg',
-    '/assets/image-product-2-thumbnail.jpg',
-    '/assets/image-product-3-thumbnail.jpg',
-    '/assets/image-product-4-thumbnail.jpg',
-  ];
+  );
   return (
     <div className="flex flex-col items-center">
-      <div className="w-72 h-72">
-        <img src="/assets/image-product-1.jpg" alt="" className="" />
-      </div>
-      <div>
+      <button className="w-80 h-80 cursor-pointer">
+        <img
+          src={activeImage}
+          alt="Active Product"
+          className="object-cover rounded-xl"
+        />
+      </button>
+      <div className="flex gap-4 mt-4">
         {thumbnail.map((src, index) => (
-          <img key={index} src={src} alt={`Thumbnail ${index + 1}`} />
+          <button
+            key={index}
+            className="border-none"
+            onClick={() => {
+              setActiveImage(images[index]);
+              setActiveThumbnail(thumbnail[index]);
+            }}
+          >
+            <img
+              src={src}
+              alt={`Thumbnail ${index + 1}`}
+              className={`w-17 h-17 object-cover rounded-lg cursor-pointer hover:opacity-70 ${
+                src === activeThumbnail
+                  ? 'ring-2 ring-orange-500 opacity-55'
+                  : ''
+              }`}
+            />
+          </button>
         ))}
       </div>
     </div>
--- app/src/components/ProductPage/ProductPage.jsx
+++ app/src/components/ProductPage/ProductPage.jsx
@@ -4,7 +4,7 @@ import ProductDetails from './ProductDetails';
 
 const ProductPage = () => {
   return (
-    <div>
+    <div className="flex flex-col md:flex-row items-center md:items-start mt-10 gap-8 p-4 justify-between">
       <ImageGallery />
       <ProductDetails />
     </div>
